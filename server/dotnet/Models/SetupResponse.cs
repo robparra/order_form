e73d11b16ebadb1b3edd727490ae0dbcfbe13f0e
Robert <parralrob@gmail.com>
2021-01-12 23:00:35 -0400

    primeros cambios

---
+++ server/dotnet/Models/SetupResponse.cs
@@ -0,0 +1,17 @@
+﻿
+using Newtonsoft.Json;
+
+public class SetupResponse
+{
+    [JsonProperty("publishableKey")]
+    public string PublishableKey { get; set; }
+
+    [JsonProperty("proPrice")]
+    public string ProPrice { get; set; }
+
+    [JsonProperty("mediumPrice")]
+    public string MediumPrice { get; set; }
+
+    [JsonProperty("basicPrice")]
+    public string BasicPrice { get; set; }
+}
